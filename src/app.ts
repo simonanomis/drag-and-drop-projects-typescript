718fe25ad59428b2bc53695fd150ebb376cf73a2
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-08-07 20:43:29 +0200

    added some 3rd party libraries for classes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import { ProjectInput } from './components/project-input';
 import { ProjectList } from './components/project-list';
+import _ from 'lodash';
+import 'reflect-metadata';
 
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
+
+console.log(_.shuffle([1, 3, 5]));
